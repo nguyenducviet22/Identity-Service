d42277fde043249bf9305c2e70af9788fb2d17e1
nguyenducviet22 <nguyenducviet22022004@gmail.com>
2025-04-15 08:58:27 +0700

    Spring Security Architecture and config to authenticate with JWT

---
+++ src/main/java/com/ndv/identity_service/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.ndv.identity_service.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final String[] PUBLIC_ENDPOINTS = {"/auth/login", "/auth/introspect", "/users"};
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(jwtConfigurer ->
+                                jwtConfigurer.decoder(jwtDecoder())))
+                .csrf(csrf -> csrf.disable()
+
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder(){
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "HS256");
+        return NimbusJwtDecoder.withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS256)
+                .build();
+    }
+
+}
